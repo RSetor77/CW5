fada71d632b33ed6d0a453a68207dbf8edee9a2c
Vasili <vasili-bik@yandex.ru>
2025-12-29 13:38:32 +0300

    Fixed functionality

--- app/src/main/java/com/example/cw5_calculator/MainActivity.kt
+++ app/src/main/java/com/example/cw5_calculator/MainActivity.kt
@@ -14,7 +14,7 @@ import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    private val operators: Array<Char> = arrayOf('+', '-', '*', '/')
+    private val operators: Array<String> = arrayOf("+", "-", "*", "/")
     private var memoryVar: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,19 +47,19 @@ class MainActivity : AppCompatActivity() {
                         child.setOnClickListener {
                             clearErr()
                             val numTextBox = findViewById<TextView>(R.id.num_text)
-                            if (numTextBox.text.toString() == "0" || numTextBox.text.toString().isEmpty())
+                            if (numTextBox.text.toString() == "0")
                             {
                                 numTextBox.text = child.text
                             } else numTextBox.text = numTextBox.text.toString().plus(child.text)
                         }
-                    else if (operators.contains(child.text.toString()[0]))
+                    else if (operators.contains(child.text.toString()))
                         child.setOnClickListener {
                             clearErr()
                             val numTextBox = findViewById<TextView>(R.id.num_text)
                             val lastChr: Char = numTextBox.text[numTextBox.text.lastIndex]
-                            if (lastChr != child.text[0] && !operators.contains(lastChr))
+                            if (lastChr != child.text[0] && !operators.contains(lastChr.toString()))
                                 numTextBox.text = numTextBox.text.toString().plus(child.text[0])
-                            else if (operators.contains(child.text[0]) && !numTextBox.text.isEmpty()) {
+                            else if (operators.contains(child.text)) {
                                 val numSub = numTextBox.text.substring(0, numTextBox.text.lastIndex)
                                 numTextBox.text =
                                     numSub.plus(child.text[0])
@@ -77,18 +77,17 @@ class MainActivity : AppCompatActivity() {
                                     findViewById<TextView>(R.id.calc_text).text = " "
                                 }
 
-                                "⌫" -> if (!numTextBox.text.isEmpty()) numTextBox.text =
-                                    numTextBox.text.substring(0, numTextBox.text.length - 1)
+                                "⌫" -> numTextBox.text = numTextBox.text.substring(0, numTextBox.text.length - 1)
 
-                                "." -> addSeparator()
+                                "." -> addSeparator(numTextBox)
                                 "√" -> {
-                                    if (!(numTextBox.text.isEmpty()) && !(operators.contains(lastChr)))
+                                    if (!(operators.contains(lastChr.toString())))
                                         numTextBox.text = numTextBox.text.toString().plus("^(1/2)")
                                 }
 
                                 "±" -> unaryMP(numTextBox)
                                 "1/X" -> {
-                                    if (!(numTextBox.text.isEmpty()) && !(operators.contains(lastChr)))
+                                    if (!(operators.contains(lastChr.toString())))
                                         numTextBox.text = numTextBox.text.toString().plus("^-1")
                                 }
 
@@ -114,7 +113,7 @@ class MainActivity : AppCompatActivity() {
 
     fun clearErr()
     {
-        if (findViewById<TextView>(R.id.num_text).text.toString() == "Ошибка выражения")
+        if (findViewById<TextView>(R.id.num_text).text.toString() == "Ошибка выражения" || findViewById<TextView>(R.id.num_text).text.toString() == "")
             findViewById<TextView>(R.id.num_text).text = "0"
     }
 
@@ -143,21 +142,19 @@ class MainActivity : AppCompatActivity() {
 
     fun eval(expression: String): String {
 
-        var result: String?
+        var result: String
         try {
             val ex: Expression = ExpressionBuilder(expression).build()
             result = ex.evaluate().toString()
         }
         catch (e: ArithmeticException)
         {
-            result = e.localizedMessage
+            result = e.message.toString()
         }
         catch (e: Exception)
         {
             result = "Ошибка выражения"
         }
-        if (result == null)
-            result = ""
         return result
     }
     fun funCalc(): Double
@@ -170,23 +167,24 @@ class MainActivity : AppCompatActivity() {
     }
     fun calc()
     {
-        findViewById<TextView>(R.id.calc_text).text = findViewById<TextView>(R.id.num_text).text.toString().plus("=")
+        val numTextBox = findViewById<TextView>(R.id.num_text)
+        findViewById<TextView>(R.id.calc_text).text = numTextBox.text.toString().plus("=")
         val result: String = eval(findViewById<TextView>(R.id.num_text).text.toString())
-        findViewById<TextView>(R.id.num_text).text = result
+        numTextBox.text = result
     }
 
-    fun addSeparator()
+    fun addSeparator(numTextBox: TextView)
     {
-        val regexString: String = arrayOf("+", "-", "*", "/", "sqrt(").joinToString("|") { Regex.escape(it) }
+        val regexString: String = operators.joinToString("|") { Regex.escape(it) }
         val regex = Regex(regexString)
-        val expression: Array<String> = findViewById<TextView>(R.id.num_text).text.toString().split(regex).toTypedArray()
+        val expression: Array<String> = numTextBox.text.toString().split(regex).toTypedArray()
         if (strIsNum(expression[expression.lastIndex]))
-            findViewById<TextView>(R.id.num_text).text = findViewById<TextView>(R.id.num_text).text.toString().plus('.')
+            numTextBox.text = numTextBox.text.toString().plus('.')
     }
 
     fun unaryMP(numTextBox: TextView)
     {
-        val regexString: String = arrayOf("+", "-", "*", "/", "sqrt(").joinToString("|") { Regex.escape(it) }
+        val regexString: String = operators.joinToString("|") { Regex.escape(it) }
         val regex = Regex(regexString)
         val expression: Array<String> = numTextBox.text.toString().split(regex).toTypedArray()
         if (isDouble(expression[expression.lastIndex]))
@@ -194,7 +192,7 @@ class MainActivity : AppCompatActivity() {
             var unChr = '-'
             try
             {
-                if (numTextBox.text.toString()[numTextBox.text.toString().lastIndexOf(expression[expression.lastIndex]) - 1] == '-')
+                if (numTextBox.text[numTextBox.text.lastIndexOf(expression[expression.lastIndex]) - 1] == '-')
                     unChr = ' '
             }
             catch (e: Exception)
@@ -202,6 +200,7 @@ class MainActivity : AppCompatActivity() {
                 unChr = '-'
             }
             val funcStr: String = (unChr + expression[expression.lastIndex]).trim()
+
             if (unChr == '-')
                 numTextBox.text = numTextBox.text.substring(0, numTextBox.text.lastIndexOf(expression[expression.lastIndex])) + funcStr
             else
